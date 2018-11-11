7fd9b07b5650c3b4a07a812924a340fe6c6798b8
Ryu Shinmura <rysestock@gmail.com>
2018-11-11 23:10:09 +0900

    初コメの文字色と背景色を設定できるようにした

--- ISitePlugin/IOptions.cs
+++ ISitePlugin/IOptions.cs
@@ -18,6 +18,8 @@ namespace SitePlugin
         FontStyle FirstCommentFontStyle { get; set; }
         FontWeight FirstCommentFontWeight { get; set; }
         int FirstCommentFontSize { get; set; }
+        Color FirstCommentBackColor { get; set; }
+        Color FirstCommentForeColor { get; set; }
 
         Color BackColor { get; set; }
         Color ForeColor { get; set; }
@@ -30,6 +32,7 @@ namespace SitePlugin
         Color BroadcastInfoForeColor { get; set; }
         Color SelectedRowBackColor { get; set; }
         Color SelectedRowForeColor { get; set; }
+
         bool IsUserNameWrapping { get; set; }
         string SettingsDirPath { get; set; }
 
